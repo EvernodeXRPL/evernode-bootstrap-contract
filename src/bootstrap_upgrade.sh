c5cff7f22781003c1c59f8ebf966414c5880525f
Chalith Desaman <desaman.chalith@gmail.com>
2022-10-20 11:03:18 +0530

    Supporting changes for environment config update (#1)
    Merging "Supporting changes for environment config update (#1)"

--- src/bootstrap_upgrade.sh
+++ src/bootstrap_upgrade.sh
@@ -124,8 +124,16 @@ function upgrade() {
             return 1
         fi
 
-        if ! append_string_field "environment"; then
-            return 1
+        env_config=$(jq '.environment' $contract_config)
+        if [ "$env_config" != "null" ]; then
+            jq -e '.environment|type!="object"' $contract_config >/dev/null &&
+                echo "Contract environment config format is invalid. Its format should be an object with string keys and string values." &&
+                return 1
+
+            jq -e '[.environment|to_entries | .[] | select(.value|type!="string") ] | any' $contract_config >/dev/null &&
+                echo "Each contract environment variable's type should be a string." &&
+                return 1
+            patch_json="$patch_json,environment:$env_config"
         fi
 
         if ! append_string_field "version" 1; then
